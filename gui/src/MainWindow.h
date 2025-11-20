e278c3bdd4213be6db9457a38b518b774fc56932
Amaresh Kumar <amaresh.kumar@live.in>
2025-11-20 10:29:06 +0100

    add gui skeleton

---
+++ gui/src/MainWindow.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <QMainWindow>
+#include <memory>
+
+class QLabel;
+class QPushButton;
+class QTimer;
+
+namespace telemetryhub::gateway { class GatewayCore; }
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow() override;
+
+private slots:
+    void onStartClicked();
+    void onStopClicked();
+    void onRefresh();
+
+private:
+    QLabel* stateLabel_ = nullptr;
+    QLabel* valueLabel_ = nullptr;
+    QPushButton* startButton_ = nullptr;
+    QPushButton* stopButton_ = nullptr;
+    QTimer* refreshTimer_ = nullptr;
+
+    // Later: either call REST or a thin client interface
+};
