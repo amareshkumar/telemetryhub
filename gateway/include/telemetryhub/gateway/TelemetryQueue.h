b9bc233e0b8a91a7d8286b60700b9c243b1decd6
Amaresh Kumar <amaresh.kumar@live.in>
2025-11-23 15:16:27 +0100

    1. add shutdown in telemetryqueue 2. modify gateway/cmakelist and top level cmakelist

--- gateway/include/telemetryhub/gateway/TelemetryQueue.h
+++ gateway/include/telemetryhub/gateway/TelemetryQueue.h
@@ -14,10 +14,14 @@ public:
     void push(const device::TelemetrySample& sample);
     std::optional<device::TelemetrySample> pop();
 
+    // Signal that no more items will be produced; unblocks waiting consumers.
+    void shutdown();
+
 private:
     std::mutex mutex_;
     std::condition_variable cv_;
     std::queue<device::TelemetrySample> queue_;
+    bool shutdown_ = false;
 };
 
 } // namespace telemetryhub::gateway
