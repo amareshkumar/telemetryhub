057f3a0086e47e41f171601f3d729a331e69a80f
Amaresh Kumar <amaresh.kumar@live.in>
2025-11-30 17:48:09 +0100

    ci(windows): Use VS 2022 generator on GitHub runners (Visual Studio 17 2022)

--- gateway/src/http_server.cpp
+++ gateway/src/http_server.cpp
@@ -1,3 +1,11 @@
+#define CPPHTTPLIB_ZSTD_SUPPORT 0
+#define CPPHTTPLIB_BROTLI_SUPPORT 0
+#define CPPHTTPLIB_ZLIB_SUPPORT 0
+#define CPPHTTPLIB_OPENSSL_SUPPORT 0
+#define CPPHTTPLIB_NO_ZSTD 1
+#define CPPHTTPLIB_NO_BROTLI 1
+#define CPPHTTPLIB_NO_ZLIB 1
+#define CPPHTTPLIB_NO_OPENSSL 1
 #include <httplib.h>
 
 #include "telemetryhub/gateway/GatewayCore.h"
