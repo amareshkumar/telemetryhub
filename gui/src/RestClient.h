54cacde212bae01278182c629a43699cb585cbed
Amaresh Kumar <amaresh.kumar@live.in>
2025-12-01 14:33:45 +0100

    [Skeleton]: 1. enable QT GUI 2. added mermaid PS scripts [errend] 3. update root cmake and gui/cmakelist 4. add main_gui source file 5. add QT window 6. add restclient h/cpp

---
+++ gui/src/RestClient.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <QObject>
+#include <QUrl>
+#include <QJsonObject>
+#include <functional>
+
+class QNetworkAccessManager;
+class QNetworkReply;
+
+class RestClient : public QObject {
+    Q_OBJECT
+public:
+    explicit RestClient(const QUrl& baseUrl, QObject* parent = nullptr);
+
+    // Callbacks receive (success payload, error string). For getStatus, success is JSON object.
+    void getStatus(std::function<void(const QJsonObject&, const QString&)> onDone);
+    void sendStart(std::function<void(bool, const QString&)> onDone);
+    void sendStop(std::function<void(bool, const QString&)> onDone);
+
+private:
+    void doPost(const QString& path, std::function<void(bool, const QString&)> onDone);
+
+    QUrl base_;
+    QNetworkAccessManager* nam_;
+};
